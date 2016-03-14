bda0a9559835be953a243903f7e01ce195845955
1510673 <1510673@rgu.ac.uk>
2016-03-14 10:44:51 +0000

    0

--- Week7/login.php
+++ Week7/login.php
@@ -18,7 +18,7 @@ if (empty($_POST["username"])||empty($_POST["password"]))
     $result=mysqli_query($db,$sql);
     if (mysqli_num_rows($result)==1)
     {
-        header("location: home.php");
+        header("location: home.html");
     }else
     {
         echo "Incorrect username or password.";
