21e61af7a2fcd69a2e09ead362b0a35b3ad376c7
1510673 <1510673@rgu.ac.uk>
2016-03-15 10:15:22 +0000

    0

--- Week7/home.php
+++ Week7/home.php
@@ -7,8 +7,9 @@
 <body>
 <h1>Hello!</h1>
 <?php  //echo $_GET["username"];
+include ("dbconnect.php");
 echo "<br>";
-$sql2="SELECT * FROM users";
+$sql2="SELECT username FROM users";
 $result2=mysqli_query($db,$sql2);
 while($row = $result2->fetch_array()) {
     $users=$row['username'];
