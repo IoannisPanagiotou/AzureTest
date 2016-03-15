4a774ed6dfd13795bc548b6c98d229dcc0e50e9a
1510673 <1510673@rgu.ac.uk>
2016-03-15 09:53:20 +0000

    0

--- Week7/home.php
+++ Week7/home.php
@@ -6,6 +6,10 @@
 </head>
 <body>
 <h1>Hello!</h1>
-<?php  echo $_GET["username"];?>
+<?php  echo $_GET["username"];
+
+$sql2="SELECT * FROM users";
+$result2=mysqli_query($db,$sql2);
+?>
 </body>
 </html>
