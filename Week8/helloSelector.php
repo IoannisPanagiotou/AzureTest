527f17d8da7ec9b33848d4073b27a98034194e4a
IoannisPanagiotou <i.panagiotou@rgu.ac.uk>
2016-03-21 10:03:44 +0000

    0

---
+++ Week8/helloSelector.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1510673
+ * Date: 21/03/2016
+ * Time: 09:55
+ */
+?>
+Where would you like to say hello?
+<br>
+
+<ul>
+    <li><a href="helloPrinter.php?planet=".$planet."">Earth</a></li>
+    <li><a href="helloPrinter.php?planet=".$planet."">Mars</a></li>
+    <li><a href="helloPrinter.php?planet=".$planet."">Uranus</a></li>
+</ul>
