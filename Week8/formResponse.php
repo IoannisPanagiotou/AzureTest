1e166ee2464a5b2405156c395c36689c788fd51e
IoannisPanagiotou <i.panagiotou@rgu.ac.uk>
2016-03-21 10:42:21 +0000

    0

---
+++ Week8/formResponse.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1510673
+ * Date: 21/03/2016
+ * Time: 10:39
+ */
+$name = $_POST[“name”];
+$gender = $_POST[“gender”];
+$power = $_POST[“power”];
+echo $name ." " .$gender." ". $power;
