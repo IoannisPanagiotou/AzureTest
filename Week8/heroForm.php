02cad59994353f4109a16fdd3090f2f17e15e4e8
IoannisPanagiotou <i.panagiotou@rgu.ac.uk>
2016-03-21 10:53:37 +0000

    0

--- Week8/heroForm.php
+++ Week8/heroForm.php
@@ -7,9 +7,9 @@
  */
 ?>
 <form action="formResponse.php" method="post">
-    <label>Name</label><input type=“text” name=“name”>
-    <label>Gender</label><input type=“text” name=“gender”>
-    <label>Main power</label><input type=“text” name=“power”>
+    <label>Name</label><input type=“text” name="name">
+    <label>Gender</label><input type=“text” name="gender">
+    <label>Main power</label><input type=“text” name="power">
     <input type="submit" value="Submit">
 </form>
 
