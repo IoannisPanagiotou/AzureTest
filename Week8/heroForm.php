729e1b1e38ebeea1673b42128a7a3f5cfc03cdcc
IoannisPanagiotou <i.panagiotou@rgu.ac.uk>
2016-03-21 10:41:13 +0000

    0

---
+++ Week8/heroForm.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1510673
+ * Date: 21/03/2016
+ * Time: 10:36
+ */
+?>
+<form action="formResponse.php" method="post">
+    <label>Name</label><input type=“text” name=“name”>
+    <label>Gender</label><input type=“text” name=“gender”>
+    <label>Main power</label><input type=“text” name=“power”>
+    <input type="submit" value="Submit">
+</form>
+
