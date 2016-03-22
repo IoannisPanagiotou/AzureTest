3d57d88d9afaf976fce71c2e4959cc22cddcafc0
1510673 <1510673@rgu.ac.uk>
2016-03-22 10:30:56 +0000

    0

--- Week8/index.php
+++ Week8/index.php
@@ -7,15 +7,15 @@
 <body>
 <h1>The Superhero System</h1>
 <h2>Superhero home page</h2>
+What would you like to do?
+<ul>
+    <li><a href="insertsuperhero.php">insert superhero</a></li>
+    <li><a href="displaySuperheros.php">display all superheros</a></li>
+</ul>
+
 <?php
 include("dbconnect.php");
 ?>
-<form action="insertsuperhero.php" method="post">
-    <label>First Name</label><input type="text" name="firstname">
-    <label>Last Name</label><input type="text" name="lastname">
-    <label>Main power</label><input type="text" name="superpower">
-    <input type="submit" value="Submit">
-</form>
 
 </body>
 </html>
