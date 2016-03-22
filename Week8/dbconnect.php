2b1c9cb3fd62d1c6e704227f719b5183665474f1
1510673 <1510673@rgu.ac.uk>
2016-03-22 10:07:19 +0000

    0

--- Week8/dbconnect.php
+++ Week8/dbconnect.php
@@ -5,7 +5,7 @@
  * Date: 22/03/2016
  * Time: 09:32
  */
-$db = new mysqli("","","","");
+$db = new mysqli("ap-cdbr-azure-east-c.cloudapp.net","b336c6b0794e0b","2c4fe3e4","IoannisPanagiotou");
 if ($db->connect_errno){
     die('Connectfailed['.$db->connect_error.']');
 }
--- Week8/formResponse.php
+++ Week8/formResponse.php
@@ -1,12 +1,25 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1510673
- * Date: 21/03/2016
- * Time: 10:39
- */
-include("dbconnect.php");
-$name = $_POST["name"];
-$gender = $_POST["gender"];
-$power = $_POST["power"];
-echo $name." ".$gender." ".$power;
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Superhero System</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<h1>The Superhero System</h1>
+<h2>Display all superheroes</h2>
+<?php  //echo $_GET["username"];
+include ("dbconnect.php");
+echo "<br>";
+$sql="SELECT * FROM superheros";
+$result=mysqli_query($db,$sql);
+while($row = $result->fetch_array()) {
+    $firstname=$row['firstName'];
+    $lastname=$row['lastName'];
+    $superpower=$row['mainSuperpower'];
+    echo "<p>" .$firstname."  ".$lastname. "  ".$superpower."</p>";
+}
+?>
+
+
+</body>
+</html>
