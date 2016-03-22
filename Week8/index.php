47b316007bae85d6186b2424cfee40195cf20eff
1510673 <1510673@rgu.ac.uk>
2016-03-22 10:24:28 +0000

    0

---
+++ Week8/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Superhero System</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<h1>The Superhero System</h1>
+<h2>Superhero home page</h2>
+<?php
+include("dbconnect.php");
+?>
+<form action="insertsuperhero.php" method="post">
+    <label>First Name</label><input type="text" name="firstname">
+    <label>Last Name</label><input type="text" name="lastname">
+    <label>Main power</label><input type="text" name="superpower">
+    <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
