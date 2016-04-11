e8bc68da5a22441e44701d63b36820b175befa93
1510673 <1510673@rgu.ac.uk>
2016-04-11 09:44:34 +0100

    0

---
+++ Week10/homepage.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1510673
+ * Date: 11/04/2016
+ * Time: 09:41
+ */
+session_start();
+$accesslevel = $_COOKIE['access_level'];
+displayAccessLevelInformation($accesslevel);
+function displayAccessLevelInformation($accessLevel) {
+    if ($accesslevel == "standarduser") {
+        echo "<p>You are currently logged in as a standard user</p>";
+    }
+    elseif ($accesslevel == "root") {
+        echo "<p>You are currently logged in as a root user</p>";
+        echo "<p>You now have access to additional administrative features</p>";
+    }
+}
+
+?>
