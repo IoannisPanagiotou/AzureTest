82a498c62bb08638e36bd7c36caf279e18db8613
1510673 <1510673@rgu.ac.uk>
2016-04-12 09:55:15 +0100

    0

---
+++ Week10/self/printGET.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1510673
+ * Date: 12/04/2016
+ * Time: 09:27
+ */
+$type=$_GET["type"];
+$colour=$_GET["colour"];
+echo "<ul>";
+    echo "<li>$type</li>";
+    echo "<li>$colour</li>";
+echo"</ul>";
