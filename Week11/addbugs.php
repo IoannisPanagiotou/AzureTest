5501cb4a16ccab6e69f53f1f365542768108f41b
1510673 <1510673@rgu.ac.uk>
2016-04-25 10:13:56 +0100

    0

--- Week11/addbugs.php
+++ Week11/addbugs.php
@@ -46,7 +46,14 @@ include("dbconnect.php");
     <?
     }
 elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
+    $bugName=$_POST["name"];
+    $bugSummary=$_POST["summary"];
+    $bugCategory=$_POST["category"];
+    $sql="INSERT INTO bugs (bugName, bugSummary, bugCategory) VALUES ('$bugName','$bugSummary','$bugCategory')";
+    if (mysql_query($db, $sql)){
+    }else{
+        echo "error: ".$sql."<br>".mysqli_error($db);
+    }
     header("location: showbugs.php");
 }
 else{
