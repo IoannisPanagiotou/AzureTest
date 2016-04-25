c5e675d84db3bfaba4c1fb2eddd73d9323bd9da8
1510673 <1510673@rgu.ac.uk>
2016-04-25 09:33:02 +0100

    0

---
+++ Week11/index.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1510673
+ * Date: 25/04/2016
+ * Time: 09:31
+ */?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>BugTracker</title>
+    <link type="text/css" rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<div id="maincontent">
+<header>
+    <img id="logo" src="logo.png">
+    <h1>BugTracker</h1>
+    <h2>Keeping track of all the pesky little bugs</h2>
+</header>
+
+<nav>
+<ul>
+    <li><a href="showbugs.html">All Bug Items</a></li>
+    <li><a href="showbugs.html">Android Bugs</a></li>
+    <li><a href="showbugs.html">iOS Bugs</a></li>
+    <li><a href="showbugs.html">Windows Bugs</a></li>
+    <li><a href="addbugs.html">Insert Bug</a></li>
+</ul>
+</nav>
+<content id="con">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+    mollit anim id est laborum.</p>
+</content>
+</div>
+<footer>
+Designed by Ioannis Panagiotou, 2016
+</footer>
+</body>
+</html>
