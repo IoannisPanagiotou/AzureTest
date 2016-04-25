a099783fc358cda87dfed1ecf53b3ee48c194be4
1510673 <1510673@rgu.ac.uk>
2016-04-25 10:03:06 +0100

    0

---
+++ Week11/dbconnect.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1510673
+ * Date: 25/04/2016
+ * Time: 10:02
+ */
+define('DB_SERVER','ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME','b336c6b0794e0b');
+define('DB_PASSWORD','2c4fe3e4');
+define('DB_DATABASE','IoannisPanagiotou');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
